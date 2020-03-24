c3ea54139a3ad9fe27a25f6f0d8c7007e1565ddd
samarche92 <samar.chehade.sc@gmail.com>
2020-03-24 14:05:35 -0400

    load and plot data

---
+++ ex2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+main file for logistic regression project
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+from matplotlib import cm
+
+
+## Load Data
+# The first two columns contains the exam scores and the third column
+# contains the label.
+
+data=np.loadtxt(fname="ex2data1.txt",delimiter=",")
+X = data[:,0:2]
+y = data[:,2]
+
+m=len(X) # number of training examples
+
+y=np.reshape(y,[m,1])  #reshaping into vector
+
+pos=np.where(y)
+pos=pos[0]
+neg=np.where(1-y)
+neg=neg[0]
+
+plt.figure()
+plt.xlabel("Exam 1 score") 
+plt.ylabel("Exam 2 score") 
+plt.plot(X[pos,0],X[pos,1],'yo') 
+plt.plot(X[neg,0],X[neg,1],'k+')
+plt.legend(['admitted','not admitted'])
+plt.show()
