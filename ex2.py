d283ac02041d0d14ba6ad6b4509fbe294d3e5ce2
samarche92 <samar.chehade.sc@gmail.com>
2020-03-24 14:42:47 -0400

    compute cost and grad

--- ex2.py
+++ ex2.py
@@ -6,8 +6,7 @@ main file for logistic regression project
 
 import numpy as np
 from matplotlib import pyplot as plt 
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-from matplotlib import cm
+import ex2_functions as fct2
 
 
 ## Load Data
@@ -19,6 +18,7 @@ X = data[:,0:2]
 y = data[:,2]
 
 m=len(X) # number of training examples
+n = np.size(X,1)
 
 y=np.reshape(y,[m,1])  #reshaping into vector
 
@@ -38,5 +38,32 @@ plt.plot(X[pos,0],X[pos,1],'yo')
 plt.plot(X[neg,0],X[neg,1],'k+')
 plt.legend(['admitted','not admitted'])
 plt.show()
+
+wait = input("Program paused. Press enter to continue \n")
+
+# Add ones to training set for the intercept term
+X=np.column_stack((np.ones([m,]),data[:,0:2])) 
+
+initial_theta=np.zeros([n+1,1]) #initialize fitting parameters
+
+# Compute and display initial cost and gradient
+(cost, grad) = fct2.costFunction(initial_theta, X, y)
+
+print('Cost at initial theta (zeros): {}\n'.format(cost))
+print('Expected cost (approx): 0.693\n')
+print('Gradient at initial theta (zeros): \n')
+print(grad)
+print('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
+
+# Compute and display cost and gradient with non-zero theta
+test_theta = np.reshape(np.array([-24, 0.2, 0.2]),[3,1])
+#test_theta = np.reshape(test_theta,[3,1]) #reshaping into vector
+(cost, grad) = fct2.costFunction(test_theta, X, y);
+
+print('\nCost at test theta: {}\n'.format(cost))
+print('Expected cost (approx): 0.218\n')
+print('Gradient at test theta: \n')
+print(grad)
+print('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n')
 
 wait = input("Program paused. Press enter to continue \n")
--- ex2_functions.py
+++ ex2_functions.py
@@ -4,3 +4,21 @@
 module containing useful functions for ex2 script
 """
 
+import numpy as np
+
+def sigmoid(z):
+    g=np.zeros(np.size(z))
+    g=1/(1+np.exp(-z))
+    return g
+
+def costFunction(theta, X, y):
+    
+    m=len(X)
+    h=sigmoid(np.matmul(X,theta))
+    J=-y*np.log(h)-(1-y)*np.log(1-h)
+    J=np.sum(J)/m
+    
+    grad=np.matmul(np.transpose(X),h-y)
+    grad=grad/m
+    
+    return (J,grad)
